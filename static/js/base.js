fdec62cf6690888497ceeee018f0144d71c73a62
Pcjmy <1716045191@qq.com>
2022-10-05 21:38:30 +0800

    game_map

--- static/js/base.js
+++ static/js/base.js
@@ -1,7 +1,10 @@
+import { GameMap } from '/static/js/game_map/base.js';
+
 class KOF {
     constructor(id) {
         this.$kof=$('#'+id);
-        console.log(this.$kof);
+        // console.log(this.$kof);
+        this.game_map = new GameMap(this);
     }
 }
 
---
+++ static/js/game_map/base.js
@@ -0,0 +1,27 @@
+import { AcGameObject } from '/static/js/ac_game_object/base.js';
+
+
+export class GameMap extends AcGameObject {
+    constructor(root) {
+        super();
+
+        this.root = root;
+        this.$canvas = $('<canvas width="100%" height="100%" tabindex=0></canvas>');
+        this.ctx = this.$canvas[0].getContext('2d');
+        this.root.$kof.append(this.$canvas);
+        this.$canvas.focus();
+    }
+
+    start() {
+
+    }
+
+    update() {
+        this.render();
+    }
+
+    render() {
+        this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+        // console.log(this.ctx.canvas.width, this.ctx.canvas.height)
+    }
+}
